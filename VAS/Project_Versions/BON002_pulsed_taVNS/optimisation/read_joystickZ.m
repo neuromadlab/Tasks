a77262b696b027958c7145c08b3922e1aaa9d0ea
neuromadlab <50832722+neuromadlab@users.noreply.github.com>
2025-01-22 15:39:28 +0100

    Delete VAS/Project_Versions/BON002_pulsed_taVNS/optimisation directory

--- VAS/Project_Versions/BON002_pulsed_taVNS/optimisation/read_joystickZ.m
+++
@@ -1,8 +0,0 @@
-count = 1;
-onset = GetSecs;
-while (GetSecs - onset) < 10
-    
-    [Joystick.X, Joystick.Y, Joystick.Z, Joystick.Button] = WinJoystickMex(JoystickSpecification);
-    posZ(count) = Joystick.Z;
-    count = count +1;
-end
